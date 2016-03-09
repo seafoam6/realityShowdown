b30290807fbe3a565828df73084142f5bad63989
Steve Barman <steve@stevebarman.com>
2016-03-09 16:23:30 -0500

    ugh

--- www/js/services/weeks.js
+++ www/js/services/weeks.js
@@ -1,16 +1,11 @@
 'use strict';
-angular.module('App').service('Weeks', function(FURL, $firebaseArray, $firebase, $log, $firebaseObject, $firebaseAuth) {
-
-  var service = this
+angular.module('App').factory('Weeks', function(FURL, $firebaseArray, $firebase, $log, $firebaseObject, $firebaseAuth) {
 
   var weeksRef = new Firebase(FURL + 'weeks');
-
-  service.getWeeks = function(){
-    
-  }
-
+  var weeks = $firebaseArray(weeksRef);
+  var Weeks = {}
   
-
+  return Weeks;
 
 
 });
--- www/views/admin/pointsController.js
+++ www/views/admin/pointsController.js
@@ -1,18 +1,29 @@
 'Use Strict';
 angular.module('App').controller('pointsController', function ($cordovaOauth, $firebaseArray, $firebaseAuth,$firebaseObject,  $http,  $ionicPopup, $localStorage, $location, $log, $scope, $state, Auth, FURL, Player, Show, Utils, Vote, Score, Weeks) {
- 
+
+
+  var votesRef;
+  var weeks;
   var weeksRef = new Firebase(FURL + 'weeks');
-  $scope.weeks = $firebaseArray(weeksRef)
+  $scope.weeks = weeks =  $firebaseArray(weeksRef)
 
   $scope.loadWeek = function(week){
     $scope.activeWeek = week
+    participationPoints.week = week.weekNumber
+    $log.log(participationPoints)
+    getVotesRef(week.weekNumber)
   }
 
-  var votesRef = new Firebase(FURL + "votes/RuPaul's%20Drag%20Race/season8/week1")
-  $scope.votes = $firebaseArray(votesRef)
+  function getVotesRef(weekNumber){
+    votesRef = new Firebase(FURL + "votes/RuPaul's%20Drag%20Race/season8/" + weekNumber).then(function(e){$log.log(e)})
 
+  }
+  
+  
+
+  var players;
   var playersRef = new Firebase(FURL + 'players')
-  $scope.players = $firebaseObject(playersRef)
+  $scope.players = players = $firebaseObject(playersRef)
 
   var getPlayers = function(){
     var names = [];
@@ -36,28 +47,8 @@ angular.module('App').controller('pointsController', function ($cordovaOauth, $f
 
   $scope.scoreParticipation = function(){
     var voterNames = getPlayers()
-    //$log.log('last', voterNames[0])
+    
 
-    //get array of player keys from twitter names
-    var playerIds = Object.keys($scope.players);
-
-    _.forEach(voterNames, function(voterName, key, collection){
-      //$log.log('value', value)
-      
-      _.forEach($scope.players, function(value2, key2, collection2){
-          //$log.log('val2', value2)
-          if(value2 !== null){
-            //$log.log('id', value2.id)
-            if (voterName == value2.id ){
-              $log.log(voterName, value2.id)
-              value2.score
-            }
-          }
-      })
-     
-      //make points block
-      //attach points block
-    })
   }
 
  })

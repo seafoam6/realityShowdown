a171a7e039b4a5a78905084cbc4b9ded7af5a6c6
seafoam6 <steve@stevebarman.com>
2016-03-09 08:18:51 -0500

    closer

--- www/js/services/weeks.js
+++ www/js/services/weeks.js
@@ -6,13 +6,7 @@ angular.module('App').service('Weeks', function(FURL, $firebaseArray, $firebase,
   var weeksRef = new Firebase(FURL + 'weeks');
 
   service.getWeeks = function(){
-    return weeksRef.once('value').then(function(snapshot) {
-      // The Promise was "fulfilled" (it succeeded).
-      return $log.log(snapshot.val());
-    }, function(error) {
-      // The Promise was rejected.
-      console.error(error);
-    });
+    
   }
 
   
--- www/views/admin/pointsController.js
+++ www/views/admin/pointsController.js
@@ -12,12 +12,12 @@ angular.module('App').controller('pointsController', function ($cordovaOauth, $f
   $scope.votes = $firebaseArray(votesRef)
 
   var playersRef = new Firebase(FURL + 'players')
-  $scope.players = $firebaseArray(playersRef)
+  $scope.players = $firebaseObject(playersRef)
 
   var getPlayers = function(){
     var names = [];
     _.forEach($scope.votes, function(o){
-      $log.log(o.$id)
+      //$log.log(o.$id)
       names.push(o.$id)
     })
     return names;
@@ -31,17 +31,30 @@ angular.module('App').controller('pointsController', function ($cordovaOauth, $f
   }
 
   function givePlayerScore(player){
-    players[0].score.push(participationPoints)
+    voterNames[0].score.push(participationPoints)
   }
 
   $scope.scoreParticipation = function(){
-    var players = getPlayers()
-    $log.log('last', players)
+    var voterNames = getPlayers()
+    //$log.log('last', voterNames[0])
 
     //get array of player keys from twitter names
+    var playerIds = Object.keys($scope.players);
 
-    _.forEach(players, function(){
-      $log.log('fk')
+    _.forEach(voterNames, function(voterName, key, collection){
+      //$log.log('value', value)
+      
+      _.forEach($scope.players, function(value2, key2, collection2){
+          //$log.log('val2', value2)
+          if(value2 !== null){
+            //$log.log('id', value2.id)
+            if (voterName == value2.id ){
+              $log.log(voterName, value2.id)
+              value2.score
+            }
+          }
+      })
+     
       //make points block
       //attach points block
     })
